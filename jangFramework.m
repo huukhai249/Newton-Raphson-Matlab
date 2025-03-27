58fbf2eedfeee6d3b4f043d64f87c8f84d4fb651
huukhai249 <khaikn249@gmail.com>
2025-03-27 22:29:49 +0900

    add error processing

--- jangFramework.m
+++ jangFramework.m
@@ -49,6 +49,7 @@ V = A*L;
 rho = 1000;
 H = d/6; %H =D/4
 kp = 0.7831;
+ks = 1.0187;
 m =10; n=0.6;
 
 Np_values = zeros(size(Q_values));
@@ -56,7 +57,7 @@ Re_values = zeros(size(Q_values));
 nloop = length(Q_values);
 mu_eff_values = zeros(size(Q_values));
 shear_rate_eff_values = zeros(size(Q_values));
-Ks_values = zeros(size(Q_values));
+% Ks_values = zeros(size(Q_values));
 
 for i = 1:nloop
     Q = Q_values(i);
@@ -68,34 +69,34 @@ for i = 1:nloop
     Re_values(i) = kp/Np_values(i);
 
     mu_eff_values(i) = (H^2*Minus_delP*Q)/(kp*V*u_avg^2);
-    shear_rate_eff_values(i) = (mu_eff_values(i)/m)^(1/(n-1));
-
-    Ks_values(i) = shear_rate_eff_values(i)/shear_rate_app;
+    % shear_rate_eff_values(i) = (mu_eff_values(i)/m)^(1/(n-1)); 
+    % Ks_values(i) = shear_rate_eff_values(i)/shear_rate_app;
+    shear_rate_eff_values(i) = ks*shear_rate_app;
 end
 
 
 % Plot each value of shear_rate_eff against mu_eff as individual points
 figure;
 
-Q_values_ml = 10^6*Q_values;
-scatter(Q_values_ml,Ks_values,'filled');
-ylim([-10, 10]);
-xlabel('Volume Flowrate,Q [ml/s]');
-ylabel('Ks [-]');
-ks=mean(Ks_values)
+% Q_values_ml = 10^6*Q_values;
+% scatter(Q_values_ml,Ks_values,'filled');
+% ylim([-10, 10]);
+% xlabel('Volume Flowrate,Q [ml/s]');
+% ylabel('Ks [-]');
+% ks=mean(Ks_values)
 % 
 % scatter(Re_values,Np_values, 'filled');
 % xlabel('Re_e_f_f [-]');
 % ylabel('N_p [-]');
 
-% scatter(shear_rate_eff_values,mu_eff_values, 'filled');
-% xlabel('shear-rate_e_f_f [1/s]');
-% ylabel('\mu_e_f_f [-]');
-% % 
+scatter(shear_rate_eff_values,mu_eff_values, 'filled');
+xlabel('shear-rate_e_f_f [1/s]');
+ylabel('\mu_e_f_f [-]');
+% 
 
 % 
 % Set logarithmic scale
 set(gca, 'XScale', 'log');
-% set(gca, 'YScale', 'log');
+set(gca, 'YScale', 'log');
 grid on;
 %%
